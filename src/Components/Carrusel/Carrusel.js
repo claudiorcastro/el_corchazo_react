3b942027b6b0f734661614ce2a2af191dcb656da
Claudio Castro <claudio.87_@hotmail.com>
2022-11-30 20:36:55 -0300

    arreglo de carrusel

--- src/Components/Carrusel/Carrusel.js
+++ src/Components/Carrusel/Carrusel.js
@@ -2,27 +2,30 @@ import React from 'react'
 
 const Carrusel = () => {
   return (
-    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+<div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
   <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+
   </div>
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.esquire.com%2Fes%2Fdonde-comer-beber%2Fg27083152%2Fmejores-whiskies-malta-del-mundo%2F&psig=AOvVaw23oOw7sKJB0KvviIQOaLh_&ust=1669934080408000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIjPtq771vsCFQAAAAAdAAAAABAE" className="d-block w-100" alt="..."/>
+      <img src="https://www.cronista.com/files/image/417/417960/61c461b492730_360_480!.jpg?s=8ead9f22e54353880f00ef93c063da0e&d=1640264381" className="d-block w-100" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cocinayvino.com%2Fvinos-bebidas%2Fcerveza-fermentados%2Fcervezas-vendidas-bares-top-mundo%2F&psig=AOvVaw2TZfeDmAyJEFmEluPYwSkr&ust=1669934043137000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKDn_I-B1_sCFQAAAAAdAAAAABAD" className="d-block w-100" alt="..."/>
+      <img src="https://latinafy.com/images/blog/22/fernet-branca__1_.jpg" className="d-block w-100" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.clarin.com%2Fgourmet%2Fcuesta-mejor-vino-argentina-importante-concurso-internacional_0_vXrvQf3Ojb.html&psig=AOvVaw3j9MV7IfwvrCiYnfVafNTF&ust=1669934003515000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLjN3puB1_sCFQAAAAAdAAAAABAD" className="d-block w-100" alt="..."/>
+      <img src="https://i.blogs.es/a80d47/foto-apertura/840_560.jpg" className="d-block w-100" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Flatinafy.com%2Fhe%2Fblog%2Fse-consigue-fernet-en-otras-ciudades-del-mundo%2F&psig=AOvVaw11RwiJI1Ee6Sl-_V9vUaI1&ust=1669933900829000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMDk7KaB1_sCFQAAAAAdAAAAABAD" className="d-block w-100" alt="..."/>
+      <img src="https://www.clarin.com/img/2021/05/26/VovFmXIGd_720x0__1.jpg" className="d-block w-100" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cronista.com%2Fclase%2Fgourmet%2Fdia-del-vino-argentino-el-mapa-con-descuentos-y-menus-especiales-para-celebrarlo-en-buenos-aires%2F&psig=AOvVaw3j9MV7IfwvrCiYnfVafNTF&ust=1669934003515000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLjN3puB1_sCFQAAAAAdAAAAABAO" className="d-block w-100" alt="..."/>
+      <img src="https://media.ambito.com/p/f91439d1dece9caf73e785dbc7246749/adjuntos/239/imagenes/040/157/0040157125/cervezajpg.jpg" className="d-block w-100" alt="..."/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
