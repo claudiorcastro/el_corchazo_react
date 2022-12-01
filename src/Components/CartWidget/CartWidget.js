410796a3ab7d3f3a6872a763a20acff9ba1448ef
Claudio Castro <claudio.87_@hotmail.com>
2022-12-01 19:07:14 -0300

    mejora del cartwidget

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const CartWidget = () => {
   return (
-    <i className="bi bi-cart-plus-fill"><span>10</span></i>
-    
-  )
-}
+    <div>
+      <a href="..."><i className="bi bi-cart-plus-fill"></i></a>
+      <span className=" cart-counter position-absolute bg-white text-center text-danger translate-middle h6 ">10</span>
+    </div>
+  );
+};
 
 export default CartWidget
