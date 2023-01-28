4f615ce9e34b023043f3c61f315e23018d4b0da2
Claudio Castro <claudio.87_@hotmail.com>
2023-01-27 22:35:23 -0300

    logo y demas

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
   return (
     <div>
-      <a href="..."><i className="bi bi-cart-plus-fill"></i></a>
+      
       <span className=" cart-counter position-absolute bg-white text-center text-danger translate-middle h6 ">10</span>
+      <Link to="/Cart"><i className="bi bi-cart-plus-fill"></i></Link>
     </div>
   );
 };
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Navbar.css'
 import CartWidget from '../CartWidget/CartWidget';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
 <nav className="navbar fondo navbar-expand-lg bg-light">
     <div className="container-fluid">
-        <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse d-flex justify-content-around" id="navbarSupportedContent">
+        <Link to="/"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6jRyxGqHqQq__7c37SgHIC0bmVhPz9TbHwUOk29zfV2xlTZSNREu2BAWSRoCkHG93flU&usqp=CAU" className="img" alt="..."/></Link>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                     <NavLink className="nav-link active" aria-current="page" to="/Error">Home</NavLink>
@@ -18,9 +19,10 @@ const Navbar = () => {
                     <NavLink className="nav-link active" aria-current="page" to="/">Catalogo</NavLink>
                 </li>
             </ul>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex">
             <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#"><CartWidget /></a>
+                    <a className="nav-link active justify-content-end" aria-current="page" href="#"><CartWidget /></a>
                 </li>
             </ul>
         </div>
